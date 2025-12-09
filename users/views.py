c5a8182eda288843b990aa9c065dc46a133aafa0
paul meyers <pmkundika@gmail.com>
2025-12-09 13:30:43 +0200

    Add: Status codes for users views

--- users/views.py
+++ users/views.py
@@ -5,31 +5,31 @@ from rest_framework.response import Response
 # Register route
 @api_view(['POST'])
 def register(request):
-      return Response({'message': 'User registration endpoint'})
+      return Response({'message': 'User registration endpoint'}, status=status.HTTP_201_CREATED)
 
 # Login route
 @api_view(['POST'])
 def login(request):
-        return Response({'message': 'User login endpoint'})
+        return Response({'message': 'User login endpoint'}, status=status.HTTP_200_OK)
 
 # Logout route
 @api_view(['POST'])
 def logout(request):
-        return Response({'message': 'User logout endpoint'})
+        return Response({'message': 'User logout endpoint'}, status=status.HTTP_200_OK)
 
 # Profile route
 @api_view(['GET'])
 def profile(request):
-        return Response({'message': 'User profile endpoint'})
+        return Response({'message': 'User profile endpoint'}, status=status.HTTP_200_OK)
 
 # Update profile route
 @api_view(['PUT'])
 def update_profile(request):
-        return Response({'message': 'Update user profile endpoint'})
+        return Response({'message': 'Update user profile endpoint'}, status=status.HTTP_200_OK)
 
 # Change password route
 @api_view(['PUT'])
 def change_password(request):
-        return Response({'message': 'Change user password endpoint'})
+        return Response({'message': 'Change user password endpoint'}, status=status.HTTP_200_OK)
 
 
